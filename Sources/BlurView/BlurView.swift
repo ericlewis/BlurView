fefb493edc801c035d02413575003aaa85d40865
ericlewis <ericlewis777@gmail.com>
2019-08-24 23:43:45 -0400

    Allow custom corner radius

--- Sources/BlurView/BlurView.swift
+++ Sources/BlurView/BlurView.swift
@@ -2,12 +2,16 @@ import SwiftUI
 
 struct BlurView: UIViewRepresentable {
     var style: UIBlurEffect.Style
+    var cornerRadius: CGFloat?
 
     func makeUIView(context: Context) -> UIVisualEffectView {
         UIVisualEffectView(effect: UIBlurEffect(style: style))
     }
     
     func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
-        // do nada
+        if let cornerRadius = self.cornerRadius {
+            uiView.clipsToBounds = true
+            uiView.layer.cornerRadius = cornerRadius
+        }
     }
 }
