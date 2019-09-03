a01fe7cd6a06d1b7bd3d76a0ab144a4fea79f5de
ericlewis <ericlewis777@gmail.com>
2019-09-03 15:37:56 -0400

    public init

--- Sources/BlurView/BlurView.swift
+++ Sources/BlurView/BlurView.swift
@@ -3,6 +3,11 @@ import SwiftUI
 public struct BlurView: UIViewRepresentable {
     public var style: UIBlurEffect.Style
     public var cornerRadius: CGFloat?
+    
+    public init(_ style: UIBlurEffect.Style, cornerRadius: CGFloat? = nil) {
+        self.style = style
+        self.cornerRadius = cornerRadius
+    }
 
     public func makeUIView(context: Context) -> UIVisualEffectView {
         UIVisualEffectView(effect: UIBlurEffect(style: style))
